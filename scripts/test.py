6cea16389ed3dac8e8dbb240a105c0796e0de759
Martin Raue <martin.raue@googlemail.com>
2012-05-04 12:38:27 +0200

    Added first test script.

---
+++ scripts/test.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import logging
+
+# Add script parent directory to python search path to get access to the pyfact package
+sys.path.append(os.path.abspath(sys.path[0].rsplit('/', 1)[0]))
+import evlio.template
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+t = evlio.template.FITSFileTemplate('templates/v1.0.0/EventList.tpl', True)
+
+for ext in t.extensions :
+    logging.info('--------------------------------------------------------------------')
+    logging.info('Found extension name={0} type={1}'.format(ext.name, ext.type_))
+    for he in ext.header :
+        logging.info('{0:>8} = {1:<20} / {2}'.format(he.key, he.value, he.comment))
