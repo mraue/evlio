d5d61c6de538733560265e57b322b2382711050c
Martin Raue <martin.raue@googlemail.com>
2012-05-15 14:40:32 +0200

    Added central Records.hh output file.

--- scripts/create_records.py
+++ scripts/create_records.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-PRE_STR = ['#include <FITSRecord.hh>\n\nnamespace FR', ' {\n\n']
+PRE_STR = ['#include <FITSRecord.hh>\n\nnamespace ', ' {\n\n']
 
 import evlio
 import evlio.utils
@@ -16,16 +16,25 @@ import tpl2record
 
 tpldict = evlio.utils.get_tpls_as_dict()
 
-outfiles = []
+recordstr = ''
 
 for tplname, verdict in tpldict.iteritems() :
+    namespace = ''
     for version, files in verdict.iteritems() :
         indextpl = evlio.BASE_PATH + '/templates/' + tplname + '/' + version + '/index.tpl'
         if os.path.isfile(indextpl) :
-            outfile =  evlio.BASE_PATH + '/src/Records_' + tplname + '_' + version.replace('.','-') + '.hh'
-            prestr = PRE_STR[0] + tplname.upper() + version.replace('.','') + PRE_STR[1]
+            outfile = 'Records_' + tplname + '_' + version.replace('.','-') + '.hh'
+            outpath = evlio.BASE_PATH + '/src/'
+            namespace = 'FITSRec' + tplname.upper() + version.replace('.','')
+            prestr = PRE_STR[0] + namespace + PRE_STR[1]
             poststr = '}\n'
-            tpl2record.tpl2record(indextpl, outfile, prestr, poststr)
-            outfiles.append(outfile)
+            tpl2record.tpl2record(indextpl, outpath + outfile, prestr, poststr)
+            recordstr += '// ' + tplname + ' ' + version + ' ' + outpath + outfile + '\n'
+            recordstr += '#include <' + outfile + '>\n'
         else :
             logging.warning('Could not open index file ' + indextpl)
+    recordstr += 'namespace FITSRec' + tplname.upper() + 'Current = ' + namespace + ';\n\n'
+
+outfile = open(evlio.BASE_PATH + '/src/Records.hh', 'w')
+outfile.write(recordstr)
+outfile.close()
