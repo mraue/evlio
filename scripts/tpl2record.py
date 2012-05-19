7da0096e0ba26046a2f337055e51f349b6df7464
Martin Raue <martin.raue@googlemail.com>
2012-05-19 14:18:02 +0200

    Added header file copy to cmake makefiles

--- scripts/tpl2record.py
+++ scripts/tpl2record.py
@@ -87,7 +87,7 @@ DATA_REC_STRUCT = [
 HEADER_REC_STRUCT = [
     '  struct recheader_', ' {\n\n', '\n  };\n\n'
     ]
-WRITE_HEADER_STRUCT = [
+WRITE_HEADER_STR = [
     '    void writeFullHeader() {\n\n', '\n    }\n\n'
     ]
 FITS_REC_STRUCT = [
@@ -161,7 +161,7 @@ def tpl2record(input_file, output_file=None, prestr='', poststr='', loglevel='IN
                         'Could not create record entry from column ' + col.type_
                         + ' in extension ' + ext.name
                         )
-            headerrecstr, writeheaderstr, readheaderstr = '', '', ''
+            headerrecstr, writeheaderstr, loadheaderstr = '', '', ''
             # Create recheader struct
             for he in ext.header :
                 if (not evlio.template._IS_TABLE_KW_RE.match(he.key) and
@@ -187,7 +187,7 @@ def tpl2record(input_file, output_file=None, prestr='', poststr='', loglevel='IN
                             + FITS_REC_STRUCT[2] + ext.name + FITS_REC_STRUCT[3] + initstr
                             + FITS_REC_STRUCT[4] + ext.name.lower() + FITS_REC_STRUCT[5]
                             + ext.name.lower() + FITS_REC_STRUCT[6]
-                            + WRITE_HEADER_STRUCT[0] + writeheaderstr + WRITE_HEADER_STRUCT[1]
+                            + WRITE_HEADER_STR[0] + writeheaderstr + WRITE_HEADER_STR[1]
                             + FITS_REC_STRUCT[7])
     outstream.write(poststr)
     if output_file :
